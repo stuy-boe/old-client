28522074dae8c8f62f3d0161c7b09d15de5b7574
Abir Taheer <abir@taheer.me>
2020-04-27 09:37:36 -0400

    replaced react sign in element with test

--- src/comps/utils/AuthButton.js
+++ src/comps/utils/AuthButton.js
@@ -1,71 +1,101 @@
 import React from 'react';
-import { GoogleLogin } from 'react-google-login';
 import AppContext from '../context/AppContext';
 import MessageQueue from '../queues/MessageQueue';
-
-import { SimpleDialog } from '@rmwc/dialog';
-import '@material/dialog/dist/mdc.dialog.css';
-import '@material/button/dist/mdc.button.css';
+import Script from 'react-load-script';
 import backend from '../../tools/backend';
 import { GOOGLE_CLIENT_ID } from '../../constants';
+import { Helmet } from 'react-helmet';
+import DialogQueue from '../queues/DialogQueue';
+import Text from '../../typography/Text';
+import { createUseStyles } from 'react-jss';
+
+const useStyles = createUseStyles({
+	GoogleButton: {
+		height: '40px'
+	}
+});
 
 const AuthButton = () => {
-	const context = React.useContext(AppContext);
-	const [payload, setPayload] = React.useState({
-		unset: true
-	});
+	const classes = useStyles();
 
-	const attemptLogin = (data = payload) => {
+	const { updateState } = React.useContext(AppContext);
+	const [buttonId] = React.useState(`auth-button-${Math.random() * 1000}`);
+
+	const attemptLogin = idToken => {
 		backend
-			.post('/api/auth/login', {
-				idToken: data.tokenId
+			.post('/api/auth/login', { idToken })
+			.then(res => {
+				if (res.data.success) {
+					updateState();
+				}
 			})
-			.then(({ data }) => {
-				context.updateState() || window.sessionStorage.clear();
-			})
-			.catch(request => {
-				if (request.response) {
+			.catch(e => {
+				if (e.response) {
 					MessageQueue.notify({
-						body: request.response.data.error.message,
+						body: e.response.data.error.message,
 						actions: [{ icon: 'close' }]
 					});
 				}
 			});
 	};
 
-	const handleSuccess = data => {
-		if (!data.profileObj.email.endsWith('@stuy.edu'))
-			return setPayload({
-				email: data.profileObj.email,
-				tokenId: data.tokenId,
-				unset: false
+	const handleSuccess = async data => {
+		const email = data.Pt.yu;
+		const idToken = data.tc.id_token;
+		let confirmation = true;
+		if (!email.endsWith('@stuy.edu')) {
+			confirmation = await DialogQueue.confirm({
+				title: 'Confirm Email',
+				body: (
+					<Text>
+						You're attempting to sign in with an email address{' '}
+						{email} that doesn't belong to the @stuy.edu
+						organization. This will limit your functionality and
+						prevent your votes from being recorded. Are you sure you
+						want to continue?
+					</Text>
+				)
+			});
+		}
+
+		if (confirmation) {
+			attemptLogin(idToken);
+		}
+	};
+
+	const renderButton = () => {
+		window.gapi.load('auth2', function () {
+			/**
+			 * Retrieve the singleton for the GoogleAuth library and set up the
+			 * client.
+			 */
+
+			const auth2 = window.gapi.auth2.init({
+				client_id: GOOGLE_CLIENT_ID
 			});
 
-		attemptLogin(data);
+			// Attach the click handler to the sign-in button
+			auth2.attachClickHandler(buttonId, {}, handleSuccess, console.log);
+		});
 	};
 
 	return (
 		<div>
-			<SimpleDialog
-				title="Confirm Email"
-				body={`You're attempting to sign in with an email address (${payload.email}) that doesn't belong to the @stuy.edu organization. This will limit your functionality and prevent your votes from being recorded. Are you sure you want to continue?`}
-				open={!payload.unset}
-				onClose={evt => {
-					if (evt.detail.action === 'accept')
-						attemptLogin({ ...payload });
-					setPayload({ unset: true });
-				}}
+			<Helmet>
+				<meta name="google-signin-scope" content="profile email" />
+				<meta
+					name="google-signin-client_id"
+					content={GOOGLE_CLIENT_ID}
+				/>
+			</Helmet>
+			<Script
+				url="https://apis.google.com/js/platform.js"
+				onLoad={renderButton}
 			/>
 
-			<GoogleLogin
-				clientId={GOOGLE_CLIENT_ID}
-				buttonText="Login with Google"
-				onSuccess={handleSuccess}
-				onFailure={er => {
-					console.log(er);
-				}}
-				cookiePolicy={'single_host_origin'}
-			/>
+			<div id={buttonId} className={classes.GoogleButton}>
+				<p>Google Auth</p>
+			</div>
 		</div>
 	);
 };
