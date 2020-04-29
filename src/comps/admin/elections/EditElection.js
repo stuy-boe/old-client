ca91c865e5af382d3c9f99f5339341bb3b0b5606
Abir Taheer <abir@taheer.me>
2020-04-29 07:57:08 -0400

    made basic election editing page

---
+++ src/comps/admin/elections/EditElection.js
@@ -0,0 +1,13 @@
+// @flow
+import React from 'react';
+
+type PropTypes = {};
+
+export const EditElection = (props:PropTypes) => {
+	return (
+		<div>
+		</div>
+	);
+};
+
+export default EditElection;
